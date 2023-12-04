fc0a6c7ade3537df431c6186fd62922a47836869
Emre <69351390+emredeveloper@users.noreply.github.com>
2023-12-04 22:56:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Temayı seç
-
-
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Seaborn kütüphanesinden tips veri setini yükleme
@@ -14,10 +12,13 @@ tips = sns.load_dataset('tips')
 # Ana başlık
 st.markdown('# Tips Veri Seti Analizi')
 
+# Sidebar'ı oluştur
+st.sidebar.header("Options")
+
 # Dropdown'lar
-sex = st.selectbox('Select Gender', ['Male', 'Female'])
-day = st.selectbox('Select Day', tips['day'].unique().tolist())
-time = st.selectbox('Select Time', ['Lunch', 'Dinner'])
+sex = st.sidebar.selectbox('Select Gender', ['Male', 'Female'])
+day = st.sidebar.selectbox('Select Day', tips['day'].unique().tolist())
+time = st.sidebar.selectbox('Select Time', ['Lunch', 'Dinner'])
 
 # Matplotlib Figürü oluşturma fonksiyonu
 def create_bar_chart(selected_data, x_feature, y_feature):
@@ -28,8 +29,8 @@ def create_bar_chart(selected_data, x_feature, y_feature):
     st.pyplot()  # Streamlit için plt.show() yerine st.pyplot() kullanılır
 
 # Save düğmesi (CSV dosyası için)
-csv_name = st.text_input('Enter CSV file name', 'selected_data')
-save_csv_button = st.button('Save as CSV')
+csv_name = st.sidebar.text_input('Enter CSV file name', 'selected_data')
+save_csv_button = st.sidebar.button('Save as CSV')
 
 # Save düğmesine tıklanma olayına tepki gösterme (CSV dosyası için)
 def save_csv():
@@ -44,36 +45,34 @@ def save_csv():
 # Dropdown değişikliklerine tepki gösterme
 selected_data = tips[(tips['sex'] == sex) & (tips['day'] == day) & (tips['time'] == time)]
 if not selected_data.empty:
-    x_feature = st.selectbox('Select X Feature', selected_data.columns.tolist())
-    y_feature = st.selectbox('Select Y Feature', selected_data.columns.tolist())
+    x_feature = st.sidebar.selectbox('Select X Feature', selected_data.columns.tolist())
+    y_feature = st.sidebar.selectbox('Select Y Feature', selected_data.columns.tolist())
     create_bar_chart(selected_data, x_feature, y_feature)
     
     # Veri seti hakkında genel istatistiksel bilgiler
-    st.write("### General Statistics")
-    st.write(selected_data.describe())
+    st.sidebar.subheader("General Statistics")
+    st.sidebar.write(selected_data.describe())
 
     # Grafik türünü seçme
-    plot_type = st.selectbox("Select Plot Type", ["Bar Plot", "Scatter Plot", "Line Plot"])
+    plot_type = st.sidebar.selectbox("Select Plot Type", ["Bar Plot", "Scatter Plot", "Line Plot"])
 
     # Seçilen grafik türüne göre görselleştirme
     if plot_type == "Bar Plot":
         create_bar_chart(selected_data, x_feature, y_feature)
     elif plot_type == "Scatter Plot":
-        st.write("### Scatter Plot")
-        st.scatter_chart(data=selected_data, x=x_feature, y=y_feature)
+        st.sidebar.subheader("Scatter Plot")
+        st.sidebar.scatter_chart(data=selected_data, x=x_feature, y=y_feature)
     elif plot_type == "Line Plot":
-        st.write("### Line Plot")
-        st.line_chart(data=selected_data)
+        st.sidebar.subheader("Line Plot")
+        st.sidebar.line_chart(data=selected_data)
 
     # Filtreleme seçenekleri
-    st.write("### Additional Filters")
-    smoker_filter = st.checkbox("Filter by Smoker")
+    st.sidebar.subheader("Additional Filters")
+    smoker_filter = st.sidebar.checkbox("Filter by Smoker")
     if smoker_filter:
         selected_data = selected_data[selected_data['smoker'] == 'Yes']
-
-    
 
     # Yeniden oluşturulan grafik
     create_bar_chart(selected_data, x_feature, y_feature)
 else:
-    st.warning("No data available for the selected filters.")
+    st.sidebar.warning("No data available for the selected filters.")
